737e2c6b5a26a4da5accd40c341ece86d40dd1ab
Alex Clement <MrClement@users.noreply.github.com>
2025-06-24 11:50:25 -0600

    Update update

--- src/main/groovy/com/morpheusdata/addon/TestAddonPackageTypeProvider.groovy
+++ src/main/groovy/com/morpheusdata/addon/TestAddonPackageTypeProvider.groovy
@@ -1,6 +1,7 @@
 package com.morpheusdata.addon
 
 import com.morpheusdata.core.MorpheusContext
+import com.morpheusdata.core.MorpheusProcessService
 import com.morpheusdata.core.Plugin
 import com.morpheusdata.core.providers.ComputeTypePackageProvider
 import com.morpheusdata.model.ComputeServerGroup
@@ -8,6 +9,12 @@ import com.morpheusdata.model.ComputeServerGroupPackage
 import com.morpheusdata.model.ComputeTypePackage
 import com.morpheusdata.model.Icon
 import com.morpheusdata.model.OptionType
+import com.morpheusdata.model.ProcessEvent
+import com.morpheusdata.model.ProcessStepType
+import com.morpheusdata.model.ProcessStepUpdate
+import com.morpheusdata.request.PackageDeleteRequest
+import com.morpheusdata.request.PackageInstallRequest
+import com.morpheusdata.request.PackageUpgradeRequest
 import com.morpheusdata.response.ServiceResponse
 
 class TestAddonPackageTypeProvider implements ComputeTypePackageProvider {
@@ -67,7 +74,7 @@ class TestAddonPackageTypeProvider implements ComputeTypePackageProvider {
 
     @Override
     String getPackageVersion() {
-        return "0.0.4"
+        return "0.0.2"
     }
 
     Icon getCircularIcon() {
@@ -89,22 +96,45 @@ class TestAddonPackageTypeProvider implements ComputeTypePackageProvider {
         ]
     }
 
+    @Override
+    ServiceResponse validatePackageConfig(ComputeServerGroup serverGroup, ComputeServerGroupPackage serverGroupPackage) {
+        def rtn = [success: true, errors: [:]]
+        if( serverGroupPackage.getConfigProperty('path')?.contains('http') ) {
+            rtn.msg = "Invalid path"
+            rtn.success = false
+        }
+        return ServiceResponse.create(rtn)
+    }
 
-    ServiceResponse<ComputeServerGroupPackage> installPackage(ComputeServerGroup serverGroup, ComputeServerGroupPackage computeServerGroupPackage) {
+    @Override
+    ServiceResponse<ComputeServerGroupPackage> installPackage(ComputeServerGroup serverGroup, ComputeServerGroupPackage computeServerGroupPackage, PackageInstallRequest packageInstallRequest) {
+        def process = packageInstallRequest.process
         println "\u001B[33mAC Log - TestAddonPackageTypeProvider:installPackage- called on ${serverGroup.name} to install ${computeServerGroupPackage.packageType.name}:${computeServerGroupPackage.packageType.packageVersion} config: ${computeServerGroupPackage.config}\u001B[0m"
         return ServiceResponse<ComputeServerGroupPackage>.success(computeServerGroupPackage)
     }
 
-    ServiceResponse deletePackage(ComputeServerGroup serverGroup,ComputeServerGroupPackage computeServerGroupPackage){
-        println "\u001B[33mAC Log - TestAddonPackageTypeProvider:deletePackage- called on ${serverGroup.name} to delete ${computeServerGroupPackage.packageType.name}:${computeServerGroupPackage.packageType.packageVersion}\u001B[0m"
-        return ServiceResponse.success("implemented")
+    @Override
+    ServiceResponse<ComputeServerGroupPackage> upgradePackage(ComputeServerGroup serverGroup, ComputeServerGroupPackage computeServerGroupPackage, PackageUpgradeRequest packageUpgradeRequest) {
+        println "\u001B[33mAC Log - TestAddonPackageTypeProvider:upgradePackage- called on ${serverGroup.name} to delete ${computeServerGroupPackage.packageType.name}:${computeServerGroupPackage.packageType.packageVersion}\u001B[0m"
+        morpheusContext.services.process.startProcessStep(packageUpgradeRequest.process, new ProcessEvent(stepType: ProcessStepType.EXECUTE_ACTION), "upgrading")
+        for (int i = 1; i <= 2; i += 1) {
+            def update = new ProcessStepUpdate(
+                    status: "update sub-step ${i}/2\n",
+                    output: "update sub-step ${i}/2\n"
+            )
+            morpheusContext.services.process.updateProcessStep(packageUpgradeRequest.process, ProcessStepType.EXECUTE_ACTION, update, true)
+
+            sleep(1000)
+        }
+        morpheusContext.services.process.endProcessStep(packageUpgradeRequest.process, MorpheusProcessService.STATUS_COMPLETE, "", true)
+        computeServerGroupPackage.packageVersion = packageUpgradeRequest.newVersion
+        return ServiceResponse<ComputeServerGroupPackage>.success(computeServerGroupPackage)
     }
 
     @Override
-    ServiceResponse<ComputeServerGroupPackage> upgradePackage(ComputeServerGroup serverGroup, ComputeServerGroupPackage computeServerGroupPackage, String newVersion) {
-        println "\u001B[33mAC Log - TestAddonPackageTypeProvider:upgradePackage- called on ${serverGroup.name} to upgrade ${computeServerGroupPackage.packageType.name}:${computeServerGroupPackage.packageType.packageVersion} to ${newVersion}\u001B[0m"
-        computeServerGroupPackage.packageVersion = newVersion
-        return ServiceResponse<ComputeServerGroupPackage>.success(computeServerGroupPackage)
-
+    ServiceResponse deletePackage(ComputeServerGroup serverGroup, ComputeServerGroupPackage computeServerGroupPackage, PackageDeleteRequest packageDeleteRequest) {
+        println "\u001B[33mAC Log - TestAddonPackageTypeProvider:deletePackage- called on ${serverGroup.name} to delete ${computeServerGroupPackage.packageType.name}:${computeServerGroupPackage.packageType.packageVersion}\u001B[0m"
+        return ServiceResponse.success()
     }
+
 }
